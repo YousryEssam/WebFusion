4dba27214e745cf2ffd698d54c9acbc85cfa69ad
YousryEssam <youssry.essam2111@gmail.com>
2024-12-28 22:46:05 +0200

    V1.5

---
+++ js/script.js
@@ -0,0 +1,24 @@
+const navbar = document.getElementById("navbar");
+window.addEventListener("scroll", () => {
+  if (window.scrollY > 50) {
+    navbar.classList.add("scrolled");
+    navbar.classList.remove("transparent-nav");
+  } else {
+    navbar.classList.remove("scrolled");
+    navbar.classList.add("transparent-nav");
+  }
+});
+
+function side_menu() {
+  const toggler = document.querySelector(".navbar-toggler");
+  if (toggler.getAttribute("aria-expanded") === "true") {
+    let show = (document.querySelector(".opn-button").style.display = "none");
+    let hide = (document.querySelector(".cls-button").style.display =
+      "inline-block");
+  } else {
+    let show = (document.querySelector(".opn-button").style.display =
+      "inline-block");
+    let hide = (document.querySelector(".cls-button").style.display = "none");
+  }
+  console.log("Hello");
+}
